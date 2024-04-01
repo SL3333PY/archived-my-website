9f86bca941366b1ebbfcefec3344fdadae0c001b
Jin-Ru Hsieh <hjrruru@gmail.com>
2024-04-01 23:38:11 +0800

    rewrite path

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">
+    <Link className="navbar-brand" to="/my_portfolio">
         <img src="/src/assets/sleepy.svg" alt="" width="28" height="28" className="d-inline-block align-text-top"/>
         <span style={{ fontFamily: 'Trebuchet MS', fontWeight: 'bold' }}>&ensp;Kim Hsieh</span>
     </Link>
@@ -14,23 +14,23 @@ const NavBar = () => {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/">
+          <Link className="nav-link" aria-current="page" to="/my_portfolio">
           <span style={{ fontFamily: "Cascadia code" }}>HOME&ensp;</span>
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">
+          <Link className="nav-link" to="/my_portfolio/about">
           <span style={{ fontFamily: "Cascadia code" }}>ABOUT&ensp;</span>
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/projects">
+          <Link className="nav-link" to="/my_portfolio/projects">
           <span style={{ fontFamily: "Cascadia code" }}>PROJECTS&ensp;</span>
           </Link>
         </li>
       </ul>
         <form className='d-flex'>
-          <Link to="/resume">
+          <Link to="/my_portfolio/resume">
             <button className="btn btn-outline-info" type="submit">
             <span style={{ fontFamily: "Cascadia code" }}>CV</span>
             </button>
